15eed9c6eab4bd95a48e6e1895f2f4d278292b24
dianluyuanli-wp <waterdoor@foxmail.com>
2017-08-15 06:23:05 -0700

    add 131

--- medium/131. Palindrome Partitioning.py
+++ medium/131. Palindrome Partitioning.py
@@ -1,23 +1,45 @@
 class Solution(object):
 	stack=[]
+	ans=[]
 	
-	@classmethod
-	def ispa(self,l,r,s):
-		while s[l]==s[r] :
-			if s[l:r+1] not in Solution.stack:
-				Solution.stack.append(s[l:r+1])
-			l-=1
-			r+=1
-			if l<0 or r>len(s)-1:
+	def rec(self,l,last,lis):
+		start=self.stack.index([l,l+1])
+		end=self.stack.index([l+1,l+2])
+		for i in self.stack[start:end]:
+			add_l=list(last)
+			add_l.append(lis[i[0]:i[1]])
+			if i[1]==len(lis):
+				self.ans.append(add_l)
+			elif self.stack.index([i[1],i[1]+1])==len(self.stack)-1:
+				add_l.append(lis[-1])
+				self.ans.append(add_l)
+			else:
+				self.rec(i[1],add_l,lis)
+			
+	def ispa(self,left,right,s):
+		while s[left]==s[right] :
+			if s[left:right+1] not in self.stack:
+				self.stack.append([left,right+1])
+			left-=1
+			right+=1
+			if left<0 or right>len(s)-1:
 				break
-	@classmethod
-	def partition(self, s):
-		for i in range(len(s)-1):
-			self.ispa(i,i,s)
-			self.ispa(i,i+1,s)
-		if s[-1] not in Solution.stack:
-			Solution.stack.append(s[-1])
-		return Solution.stack
 	
-aa='aab'
+	def partition(self, s):
+		self.stack=[]
+		self.ans=[]
+		if len(s)==1:
+			a=[]
+			a.append(s)
+			self.ans.append(a)
+			return self.ans
+		for i in range(len(s)):
+			self.ispa(i,i,s)
+			if i!=len(s)-1:
+				self.ispa(i,i+1,s)
+		self.stack.sort()
+		self.rec(0,[],s)
+		return self.ans
+	
+aa="cbbc"
 print Solution().partition(aa)
